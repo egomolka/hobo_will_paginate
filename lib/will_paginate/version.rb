8973a55b943c99fd0421ef28a2e8fe3453ce1a26
Bryan Larsen <bryan@larsen.st>
2012-10-10 04:36:17 -0400

    change version to 3.0.3.hobo so that we can explicitly depend on our fork.

--- lib/will_paginate/version.rb
+++ lib/will_paginate/version.rb
@@ -2,8 +2,8 @@ module WillPaginate #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 3
     MINOR = 0
-    TINY  = 3
+    TINY  = '3.hobo'
 
-    STRING = [MAJOR, MINOR, TINY].join('.')
+    STRING = '3.0.3.hobo'
   end
 end
